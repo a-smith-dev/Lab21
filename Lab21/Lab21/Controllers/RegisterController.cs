53bf09356b71dce2b5ecd16d557e5e4049d76e8d
andy <asmithdev91@gmail.com>
2020-07-15 16:42:31 -0400

    first commit

---
+++ Lab21/Lab21/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab21.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab21.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddUser(Register register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(register);
+        }
+    }
+}
